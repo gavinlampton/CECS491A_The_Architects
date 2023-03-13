2ce51e7273c073824ed7166ffed14a33807a76c0
Manpunch <Henry.Yeom01@student.csulb.edu>
2023-03-13 03:57:31 -0700

    Function to input data into customer table

--- server/database.js
+++ server/database.js
@@ -2,14 +2,35 @@ const mysql = require('mysql2');
 let conn = mysql.createConnection({
     user: "root",
     host: "localhost",
-    password: "password",
+    password: "12345",
     database: "customer_notifications"
   });
 
-conn.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL Server!');
-})
-
-
 exports = module.exports = conn;
+
+var first_name = 'H';
+var last_name = 'A';
+var email = '@gmail.com';
+var phone_number = '123121411';
+var sale_notification = 1;
+var newstock_notification = 0;
+
+addcustomer(first_name, last_name, email, phone_number, sale_notification, newstock_notification);
+function addcustomer(first_name, last_name, email, phone_number, sale_notification, newstock_notification){
+  //connected to MySQL Database
+  conn.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL Server!');
+  })
+
+  //SQL Statement
+  var sql = "INSERT INTO customer (first_name, last_name, email, phone_number, sale_notification, newstock_notification) " + 
+  "VALUES ('"+first_name+"','"+last_name+"','"+email+"','"+phone_number+"',"+sale_notification+", "+newstock_notification+")";
+  conn.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+
+  //end connection so you can use Terminal Afterwards
+  conn.end();
+}
