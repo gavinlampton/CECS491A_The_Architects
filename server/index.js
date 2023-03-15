2e8dbaa983dc722eb14250365d3f8832f84ebf36
usamapal01 <usamapal01@users.noreply.github.com>
2023-03-14 21:17:24 -0700

    made it modular
    I seperated files

--- server/index.js
+++ server/index.js
@@ -1,38 +1,17 @@
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const customerRouter = require('./routes/customerForm');
+
 const app = express();
-const mysql = require('mysql2');
-const conn = require('./database');
 
-// Use CORS middleware
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/customer', customerRouter);
 
-// Parse JSON body in POST requests
-app.use(express.json());
+const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send('Welcome to home page!');
-});
-
-// Handle POST request from the customer form
-app.post("/customer", (req, res) => {
-  const { first, last, email, phone, saleNotifications, stockNotifications } = req.body;
-
-  // Insert customer data into the database
-  const sql = `INSERT INTO customer (first_name, last_name, email, phone_number, sale_notification, newstock_notification)
-               VALUES (?, ?, ?, ?, ?, ?)`;
-  const values = [first, last, email, phone, saleNotifications, stockNotifications];
-  conn.query(sql, values, (err, result) => {
-    if (err) {
-        console.error(err.message);
-      res.status(500).send('Error saving customer data to database');
-    } else {
-      console.log(`Customer data saved to database with ID ${result.insertId}`);
-      res.sendStatus(200);
-    }
-  });
-});
-
-app.listen(3001, () => {
-  console.log("Server listening on 3001");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
